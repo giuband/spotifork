bb7901012ffa8cbf6ab14008b927acdf2bdaad60
giuband <giuseppe.bandiera@live.com>
2017-12-03 13:45:03 +0100

    Add redis cache

--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
 const pitchfork = require('pitchfork');
 const express = require('express');
 const path = require('path');
-const fs = require('fs')
+const fs = require('fs');
+const redis = require('redis');
+const bluebird = require('bluebird');
+
+bluebird.promisifyAll(redis.RedisClient.prototype);
+bluebird.promisifyAll(redis.Multi.prototype);
+const redisClient = redis.createClient(6379, 'redis');
 
 const { searchAlbumOnSpotify } = require('./spotify');
 
@@ -20,12 +26,23 @@ app.use(function(req, res, next) {
 
 app.param('page', (req, res, next, page) => {
   try {
-    const pitchforkRequest = new pitchfork.Page(page);
-    pitchforkRequest.on('ready', () => {
-      const { results } = pitchforkRequest;
-      const reviews = results.map(res => res.attributes);
-      res.json({ reviews });
-    });
+    redisClient.getAsync(`page${page}`).then(results => {
+      if (!results) {
+        console.log(`cache miss for pitchfork page ${page}`)
+        const pitchforkRequest = new pitchfork.Page(page);
+        pitchforkRequest.on('ready', () => {
+          const { results } = pitchforkRequest;
+          const reviews = results.map(res => res.attributes);
+          redisClient.set(`page${page}`, JSON.stringify(reviews))
+          redisClient.expire(`page${page}`, 60 * 60 * 24)
+          res.json({ reviews });
+        });
+      } else {
+        console.log(`cache hit for pitchfork page ${page}`)
+        const reviews = JSON.parse(results)
+        res.json({ reviews })
+      }
+    })
   } catch (err) {
     res.status(500).send({
       error: 'There was an error while fetching data from pitchfork ',
@@ -39,16 +56,25 @@ app.get('/getPage/:page', (req, res, next) => {
 
 app.get('/search', (req, res) => {
   const { artist, album } = req.query;
-  searchAlbumOnSpotify({ artist, album })
-    .then(data => {
-      const result = data.body.albums && data.body.albums.items[0];
-      if (result) {
-        res.json(result);
-      } else {
-        res.json({ error: 'Not available on spotify' });
-      }
-    })
-    .catch(() => res.json({ error: 'An error occured' }));
+  redisClient.getAsync(`${artist}:${album}`).then(result => {
+    if (!result) {
+      console.log(`cache miss for ${artist}:${album}`)
+      searchAlbumOnSpotify({ artist, album })
+      .then(data => {
+        const result = data.body.albums && data.body.albums.items[0];
+        if (result) {
+          redisClient.set(`${artist}:${album}`, JSON.stringify(result))
+          res.json(result);
+        } else {
+          res.json({ error: 'Not available on spotify' });
+        }
+      })
+      .catch(() => res.json({ error: 'An error occured' }));
+    } else {
+      console.log(`cache hit for ${artist}:${album}`)
+      res.json(JSON.parse(result))
+    }
+  })
 });
 
 app.get('/', (req, res) => {
